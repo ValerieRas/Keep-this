b19bff123c6209ab146aea5fa3e86ce5650314cb
Aura974 <aurore.ducap@gmail.com>
2024-08-13 15:46:07 +0200

    refactor(db context): move connection string management to configuration

--- API.KeepThis/API.KeepThis/Data/KeepThisDbContext.cs
+++ API.KeepThis/API.KeepThis/Data/KeepThisDbContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using API.KeepThis.Model;
+﻿using API.KeepThis.Model;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.KeepThis.Data;
@@ -34,9 +32,6 @@ public partial class KeepThisDbContext : DbContext, IKeepThisDbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=keep-this;Username=postgres;Password=Tartu44e!;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
